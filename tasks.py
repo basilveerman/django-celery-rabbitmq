213b95e115adde97c86b0e7831c862dc45b93433
Basil Veerman <bveerman@uvic.ca>
2016-02-12 11:15:06 -0800

    Python3 compatibility fixes, add docker celery command

--- tasks.py
+++ tasks.py
@@ -4,15 +4,15 @@ app = Celery('tasks', backend='amqp', broker='amqp://')
 
 @app.task(ignore_result=True)
 def print_hello():
-    print 'hello there'
+    print('hello there')
 
 @app.task
 def gen_prime(x):
     multiples = []
     results = []
-    for i in xrange(2, x+1):
+    for i in range(2, x+1):
         if i not in multiples:
             results.append(i)
-            for j in xrange(i*i, x+1, i):
+            for j in range(i*i, x+1, i):
                 multiples.append(j)
     return results
